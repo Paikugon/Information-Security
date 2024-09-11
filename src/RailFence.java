93d16702adc7ee82f6fd1bc06cb020438ff73b56
Phạm Khắc Duy Long <longphamkhacduy@gmail.com>
2024-09-11 14:17:05 +0700

    added user input key

--- src/RailFence.java
+++ src/RailFence.java
@@ -2,7 +2,14 @@
 This is the code for Rail Fence cipher technique (2.3.1 in the book)
 see more about it here: https://en.wikipedia.org/wiki/Rail_fence_cipher
 the article says that there are three rails in the cipher method, but
-we are taught to use 2 in school book, so this program only use 2 rows as you can see below
+we are taught to use  custom keys in school book, so this program uses custom keys, too
+At uni, we were taught to do as following:
+plain text: abcdef
+rewriting in Rail Fence (with 3 rows):
+a . . d . .
+. b . . e .
+. . c . . f
+-> cipher: adbecf
 if you have any questions/suggestions, feel free to contact us about it
 Thanks for reading this, enjoy the rest of your day :)
 */
@@ -10,54 +17,44 @@ Thanks for reading this, enjoy the rest of your day :)
 import java.util.Scanner;
 
 public class RailFence {
-    protected static String encrypt(String Plain){
+    protected static String encrypt(String Plain, int NumKey){
         String Encrypted = "";
         int n = Plain.length();
-        char [][]rewrite = new char[2][n / 2 + n % 2];
 
-        //write on two rows
-        //Ex: abcde will become
-        //a c e
-        //b d
-        for (int i = 0; i < n; i++) {
-            rewrite[i % 2][i / 2] = Plain.charAt(i);
-        }
-
-        //iterate 2 rows and add them to Encrypted string
-        for (int i = 0; i <= 1; i++){
-            for (int j = 0; j < n/2 + n % 2 - i*(n%2); j++){
-                Encrypted += rewrite[i][j];
+        /* the indexes of the numbers on the same row will have the same remainder when modulo NumKey
+        so we iterate each remainders and add corresponding characters to Encrypted string
+         */
+        for (int i = 0; i < NumKey; i++){
+            for (int j = i; j < n; j += NumKey){
+                Encrypted += Plain.charAt(j);
             }
         }
         return Encrypted;
     }
 
-    protected static String decrypt(String Encrypted){
+    protected static String decrypt(String Encrypted, int NumKey){
         String Decrypted = "";
         int n = Encrypted.length();
-        char [][]rewrite = new char[2][n / 2 + n % 2];
 
-        int cnt = 0;
-        //write on two rows
-        //write on two rows
-        //Ex: acebd will become
-        //a c e
-        //b d
-        for (int i = 0; i <= 1; i++) {
-            for (int j = 0; j < n/2 + n % 2 - i*(n%2); j++){
-                rewrite[i % 2][j] = Encrypted.charAt(cnt);
-                cnt++;
+        //max colum in the original message table, and cur will keep track of the current character in Encrypted text
+        int maxCol = (int)Math.ceil((float)n / NumKey);
+        int cur = 0;
+        char[][] tempArr = new char [NumKey][maxCol];
+        for (int i = 0; i < NumKey; i++){
+            int col = n / NumKey;
+            if (i < (n % NumKey))
+                col++;
+            for (int j = 0; j < col; j++){
+                tempArr[i][j] = Encrypted.charAt(cur);
+                cur++;
             }
         }
-
-        //iterate 2 rows and add them to Decrypted string
-        for (int j = 0; j < n/2; j++){
-            Decrypted += rewrite[0][j];
-            Decrypted += rewrite[1][j];
+        for (int j = 0; j < maxCol; j++){
+            for (int i = 0; i < NumKey; i++){
+                if (tempArr[i][j] >= 'a' && tempArr[i][j] <= 'z')
+                    Decrypted += tempArr[i][j];
+            }
         }
-        //add the last column to Decrypted if the number of characters is odd
-        if (n % 2 != 0)
-            Decrypted += rewrite[0][n/2];
         return Decrypted;
     }
 
@@ -65,10 +62,12 @@ public class RailFence {
         Scanner sc = new Scanner(System.in);
         System.out.print("Input Plain text: ");
         String PlainText = sc.nextLine();
+        System.out.print("Input number key: ");
+        int NumKey = sc.nextInt();
 
-        String Encrypted = encrypt(PlainText);
+        String Encrypted = encrypt(PlainText, NumKey);
         System.out.println("Encrypted text: " + Encrypted);
-        String decrypted = decrypt(Encrypted);
+        String decrypted = decrypt(Encrypted, NumKey);
         System.out.println("Decrypted text: " + decrypted);
     }
 }
